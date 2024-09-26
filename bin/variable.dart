185135adbb7247bc394e3456f9a618ad6d0c5b62
ekokurniawann <ekokurniawaann@gmail.com>
2024-09-26 09:41:03 +0700

    update variabel

--- bin/variable.dart
+++ bin/variable.dart
@@ -1,38 +1,84 @@
 void main() {
-  // Deklarasi variabel
-  String name;
-  name = 'Eko kurniawan';
+  // 1.Deklarasi variabel dengan tipe data eksplisit
+  String name = 'Eko Kurniawan';
+  int age = 25;
+  bool isStudent = true;
 
-  // Deklarasi langsung
-  String username = 'echo';
+  print('Nama: $name');
+  print('Umur: $age');
+  print('Apakah mahasiswa? $isStudent');
 
-  // Keyword var
-  var age = 18;
+  // 2. Penggunaan 'var' untuk varibel yang tipe datanya di-infer otomatis
+  var username = 'echo';
+  var height = 175.5; // ini tipe nya double
 
-  print(name);
-  print(username);
-  print(age);
+  print('Username: $username');
+  print('Tinggi: $height');
 
-  // final
-  var firtsName = 'Eko';
+  // 3. final : nilai hanya bisa diinisialisasi sekali
+  final firstName = 'Eko';
   final lastName = 'Kurniawan';
 
-  print(firtsName);
-  print(lastName);
+  print('Nama depan: $firstName');
+  print('Nama belakang: $lastName');
 
-  // const
-  final array1 = [1, 2, 3];
-  const array2 = [1, 2, 3];
+  // Error : tidak bisa mengubah nilai final
+  //firstName = 'Roger';
 
-  print(array1);
-  print(array2);
+  // 4. const : nilai tetap yang di ketahui saat compile-time
+  const pi = 3.14;
+  const maxItems = 100;
 
-  late var value = getValue();
-  print('Variable sudah dibuat');
-  print(value);
+  print('Pi: $pi');
+  print('Max Items: $maxItems');
+
+  // 5. final and const
+  final currentTime = DateTime.now(); // final bisa di inisialisasi saat runtime
+  // const compileTime = DateTime.now(); // Error : const harus di compile-time
+
+  print('Waktu sekarang: $currentTime');
+
+  // 6. late : inisialisasi variabel di tunda sampai di gunakan
+  late String description;
+  description = getDescription();
+  print('Deskripsi: $description');
+
+  // 7. tipe data list dan map
+  List<int> numbers = [1, 2, 3, 4, 5];
+  Map<String, String> user = {
+    'name': 'Eko',
+    'role': 'Software Enginer',
+  };
+
+  print('List numbers: $numbers');
+  print('User info: ${user['name']}');
+
+  // 8. scope variabel
+  var globalVariable = 'Ini variable global';
+  print(globalVariable);
+
+  // local scope
+  void localScope() {
+    var localVariabel = 'Ini variabel lokal';
+    print(localVariabel);
+  }
+
+  localScope();
+  // print(localVariable) // Error : tidak bisa di akses di luar scope
+
+  // 9. Mutability vs Immutability
+  var mutableVariable = 'Bisa diubah';
+  mutableVariable = 'Sudah diubah';
+
+  final immutableVariable = 'Tidak bisa diubah';
+  //immutableVariable = 'Error'; // Error : tidak bisa mengubah nilai final
+
+  print('Mutable: $mutableVariable');
+  print('Immutbale: $immutableVariable');
 }
 
-String getValue() {
-  print('getValue() dipanggil');
-  return 'Eko kurniawan';
+// fungsi untuk late inisialisasi
+String getDescription() {
+  print('getDescription() dipanggil');
+  return 'Dart is awesome';
 }
